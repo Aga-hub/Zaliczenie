7bfab5363fb40f13c58072c3a9865896ece12706
agnieszka.gajda <agnieszka.gajda.99@gmail.com>
2023-06-22 16:27:52 +0200

    First commit

---
+++ src/main/java/GUI/Main.java
@@ -0,0 +1,10 @@
+package GUI;
+
+public class Main {
+
+    public static void main (String args[]){
+        System.out.println("I prefer C#, so tbh I am going to hevily rely on my old java project since I do not remember anything from this language");
+    }
+
+
+}
